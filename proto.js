416a9a22aa2d1f5e56388c69c68e6413ebd8cf64
Vladimir Zyuzin <128633448+VlaffiGG@users.noreply.github.com>
2023-07-21 22:15:50 +0200

    kleiner Patch
    -bessere Veranschaulichung des Codes 
    -Update bezogen auf das Aussehen "leerer" Zeilen für Testzwecke
    -Grammatik in Kommentaren

--- proto.js
+++ proto.js
@@ -11,18 +11,18 @@ function erstelle2DArray(spalten, reihen) {
 var spalten = 10;
 var reihen = 10;
 var netz;  // globale Variable
-var w = 20;  // globale Variable, jede Zeile ist 20x20 Pixel groß
+var z = 20;  // globale Variable, jede Zeile ist 20x20 Pixel groß
 
 function setup() {
-    createCanvas(161, 161);
-    spalten = floor(width/w);  // berechnet Anzahl der Spalten und verhindert float Werte
-    reihen = floor(height/w);  // berechnet Anzahl der Reihen
+    createCanvas(161, 161);  // 1 Pixel größer, damit unten und rechts ein Rahmen erscheint
+    spalten = floor(width/z);  // berechnet Anzahl der Spalten und verhindert float Werte
+    reihen = floor(height/z);  // berechnet Anzahl der Reihen
     netz = erstelle2DArray(spalten, reihen);
     for (var i = 0; i < spalten; i++) {
         for (var j = 0; j < reihen; j++) {
-            netz[i][j] = new Zeile(i*w, j*w, w);  // berechnet Pixel Standort jeder Zeile
+            netz[i][j] = new Zeile(i*z, j*z, z);  // berechnet Pixel Standort jeder Zeile
         } // das Spielfeld besteht aus einem Netz von Zeilenobjekten
-    }  // Spalten können als x, und Reihen können als y verbildlicht werden
+    }  // Spalten können als x und Reihen können als y verbildlicht werden
 }  // fuer jede Spalte und Reihe wird im Loop eine Zeile erstellt
 
 function mousePressed() {
@@ -46,33 +46,37 @@ function draw() {
 
 // Code für Zeilen
 
-function Zeile(x, y, w) {  //Objekt für Zeilen
+function Zeile(x, y, z) {  //Objekt für Zeilen
     this.x = x;
     this.y = y;
-    this.w = w;
+    this.z = z;
     if (random(1) < 0.5) {
         this.mine = true;
     } else {
         this.mine = false;
-    }  // dies verteilt Minen zufällig auf 10% der Felder
+    }  // dies verteilt Minen zufällig auf 50% der Felder
     this.aufgeloest = false;
 }  // unter diesen Bedingungen sind alle Zeilen verdeckt
 
 Zeile.prototype.show = function() {
     stroke(0);
     noFill();
-    rect(this.x, this.y, this.w, this.w);
+    rect(this.x, this.y, this.z, this.z);
     if (this.aufgeloest) {
         if (this.mine) {
-            square(this.x + this.w * 0.3, this.y + this.w * 0.3, this.w * 0.4);
+            fill(127);
+            square(this.x + this.z * 0.3, this.y + this.z * 0.3, this.z * 0.4);
+        } else {
+            fill(200);
+            rect(this.x, this.y, this.z, this.z);
         }
     }
 }
 
 Zeile.prototype.contains = function(x, y) {
-    return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w);
-} //  das Identifiziert, was genau als Zeile angesehen wird
+    return (x > this.x && x < this.x + this.z && y > this.y && y < this.y + this.z);
+} //  das identifiziert, was genau als Zeile angesehen wird
 
 Zeile.prototype.aufloesen = function() {
     this.aufgeloest = true;
-} //  damit werden die Minen veranschaulicht
+} //  damit wird der Zeileninhalt veranschaulicht
